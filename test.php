ec634cd1fbe75871491916864a3936783ce8fe8f
nkmeena94 <nkmeena101@gmail.com>
2017-01-18 23:40:24 +0530

    files

---
+++ test.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Vertical (basic) form</h2>
+  <form>
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+    </div>
+    <div class="checkbox">
+      <label><input type="checkbox"> Remember me</label>
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
+  </form>
+</div>
+
+</body>
+<script type="text/javascript">
+console.log("HEllo");
+	$( document ).ready(function() {
+  
+  var merchant_id = 3;
+  var api_url = "http://localhost:1203/api/xeno/campaigns/getAllCampaigns"+"/"+merchant_id;
+
+  $.ajax({url: api_url, success: function(result){
+  	console.log(result);
+        
+    }});
+
+});
+</script>
+</html>
+
+
